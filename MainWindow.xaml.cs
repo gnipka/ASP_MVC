553db0416a60f696046593e942a669d7410b9ed9
gnipka <gnipka29@gmail.com>
2022-10-23 21:21:01 +0300

    task 1, 2 are done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ASP_MVC
 {
@@ -23,6 +13,43 @@ namespace ASP_MVC
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddNumberFibonacci()
+        {
+            int a = 0;
+            int b = 1;
+            int time = 0;
+
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background,new Action(() =>
+            {
+                tbFib.Text += a + " ";
+                time = int.Parse(tbTime.Text);
+            }));
+            Thread.Sleep(time * 1000);
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+            {
+                tbFib.Text += b + " ";
+                time = int.Parse(tbTime.Text);
+            }));
+
+            while (true)
+            {
+                a = (b+=a)-a;
+                Thread.Sleep(time * 1000);
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    tbFib.Text += a + " ";
+                    time = int.Parse(tbTime.Text);
+                }));                
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(AddNumberFibonacci));
+            thread.Start();
+
         }
     }
 }
