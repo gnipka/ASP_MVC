fca8db908cc47c06ffd6616f7099542dcb2301e8
gnipka <gnipka29@gmail.com>
2022-10-23 21:41:02 +0300

    task 3 is done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,35 +21,71 @@ namespace ASP_MVC
             int b = 1;
             int time = 0;
 
-            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background,new Action(() =>
+            try
             {
-                tbFib.Text += a + " ";
-                time = int.Parse(tbTime.Text);
-            }));
-            Thread.Sleep(time * 1000);
-            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
-            {
-                tbFib.Text += b + " ";
-                time = int.Parse(tbTime.Text);
-            }));
-
-            while (true)
-            {
-                a = (b+=a)-a;
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                 {
+                     tbFib.Text += a + " ";
+                     try
+                     {
+                         time = int.Parse(tbTime.Text);
+                         tbError.Text = String.Empty;
+                     }
+                     catch (FormatException)
+                     {
+                         time = 2;
+                         tbError.Text = "Введите целое число";
+                     }
+                 }));
                 Thread.Sleep(time * 1000);
                 Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
                 {
-                    tbFib.Text += a + " ";
-                    time = int.Parse(tbTime.Text);
-                }));                
+                    tbFib.Text += b + " ";
+                    try
+                    {
+                        time = int.Parse(tbTime.Text);
+                        tbError.Text = String.Empty;
+                    }
+                    catch (FormatException)
+                    {
+                        time = 2;
+                        tbError.Text = "Введите целое число";
+                    }
+                }));
+
+                while (true)
+                {
+                    a = (b+=a)-a;
+                    Thread.Sleep(time * 1000);
+                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    {
+                        tbFib.Text += a + " ";
+                        try
+                        {
+                            time = int.Parse(tbTime.Text);
+                            tbError.Text = String.Empty;
+                        }
+                        catch(FormatException)
+                        {
+                            time = 2;
+                            tbError.Text = "Введите целое число";
+                        }
+                    }));
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    MessageBox.Show("Поток был прерван", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                }));
             }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Thread thread = new Thread(new ThreadStart(AddNumberFibonacci));
-            thread.Start();
-
+            thread.Start();            
         }
     }
 }
